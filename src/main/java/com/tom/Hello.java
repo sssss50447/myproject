f88b71613209e5f845c6143a55ad6580db6b0fb0
sssss50447 <sssss50447@gmail.com>
2019-03-11 04:08:04 +0800

    Java and Kotile variable, data type

--- src/main/java/com/tom/Hello.java
+++ src/main/java/com/tom/Hello.java
@@ -5,5 +5,15 @@ public class Hello {
 //        System.out.println("Hello word");
         Person p = new Person();
         p.hello();
+        int age = 19;
+        Integer age2 = 19;
+        char c = '我';
+        Character c2 = 'A';
+        byte b = 120;
+        float weight = 66.5f;
+        double height = 1.7;
+        boolean adult = true;
+        boolean enroll = false;
+        String name = "Tom";
     }
 }
